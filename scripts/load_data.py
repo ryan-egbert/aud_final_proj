bb2a856e5396d54f51f48671fb54df167dd0ac6a
Ryan Egbert <59709130+ryan-egbert@users.noreply.github.com>
2021-12-08 23:10:44 -0500

    Removed extra /

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -4,7 +4,7 @@ import csv
 with open('../pck/cl_data.pck', 'rb') as f:
     data = pck.load(f)
 
-with open('..//csv/data.csv', 'w') as f:
+with open('../csv/data.csv', 'w') as f:
     writer = csv.writer(f)
     for row in data:
         if type(row) != str:
